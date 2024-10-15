e8963a4a02a72db23dc87caf36a8a9d699fabadf
1prk <iprk@protonmail.com>
2024-10-15 07:11:08 +0000

    enable CSV writing

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,86 @@
 mod osm_conditions;
 mod assessor;
 
-use osm_io::osm::model::element::Element;
-use osm_io::osm::pbf;
-use osm_io::osm::pbf::compression_type::CompressionType;
-use osm_io::osm::pbf::file_info::FileInfo;
+use osmpbf::{ElementReader, Element};
 
 use std::collections::HashMap;
 use assessor::Assessor;
 use crate::osm_conditions::Conditions;
+use geo::{coord};
+use csv::Writer;
 
-use std::path::PathBuf;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = "data/Leipzig_osm.osm.pbf";
 
-use anyhow;
-// use benchmark_rs::stopwatch::StopWatch;
-// use simple_logger::SimpleLogger;
+    // ähnlich einer dict mit der osm_id als integer_64, einer weiteren hashmap mit kv-pairs der attribute
+    let mut node_coords: HashMap<i64, (f64, f64)> = HashMap::new();
 
-pub fn main() -> Result<(), anyhow::Error> {
-    // SimpleLogger::new().init()?;
-    log::info!("Started pbf io pipeline");
-    // let mut stopwatch = StopWatch::new();
-    // stopwatch.start();
-    let input_path = PathBuf::from("./data/Leipzig_osm.osm.pbf");
-    let output_path = PathBuf::from("./data/Leipzig_osm_bicycle_infra.osm.pbf");
-    let reader = pbf::reader::Reader::new(&input_path)?;
-    let mut file_info = FileInfo::default();
-    file_info.with_writingprogram_str("pbf-io-example");
-    let mut writer = pbf::writer::Writer::from_file_info(
-        output_path,
-        file_info,
-        CompressionType::Zlib,
-    )?;
+    let reader = ElementReader::from_path(path)?;
 
-    writer.write_header()?;
+    let mut wtr = Writer::from_path("./data/leipzig_assessed.csv")?;
+    wtr.write_record(&["osm_id", "bicycle_infrastructure", "WKT"])?;
 
-    for element in reader.elements()? {
-        if let Element::Way { way } = &element {
-            if way.tags().any(|tag| tag.k() == "highway") {
-                let mut tags: HashMap<String, String> = way.tags().
-                    map(|tag| (tag.k().to_owned(), tag.v().to_owned())).
-                    collect();
-
-                let mut assessor = Assessor {
-                    conditions: Conditions { tags: &mut tags },
-                };
-                assessor.assess();
-
-                writer.write_element(Element::Way { way: way })?;
-                continue;
-            }
+    // lese alle nodes aus um geometrien zu sammeln
+    reader.for_each(|element| {
+        if let Element::DenseNode(node) = element {
+            let lat = node.lat();
+            let lon = node.lon();
+            node_coords.insert(node.id(), (lat, lon));
         }
-        writer.write_element(element)?;
-    }
+    })?;
 
-    writer.close()?;
+    // lese alle ways aus mit ihren tags
+    let reader = ElementReader::from_path(path)?; // Re-open the reader
 
-    // log::info!("Finished pbf io pipeline, time: {}", stopwatch);
+    reader.for_each(|element| {
+        if let Element::Way(way) = element {
+            if !way.tags().any(|(key, _)| key == "highway") {
+                return;
+            }
+
+
+            // way-tags sammeln
+            let mut tags: HashMap<_, _> = way.tags()
+                .map(|(key, value)| (key.to_owned(), value.to_owned()))
+                .collect();
+
+            // assessor initialisieren und starten
+            let mut assessor = Assessor { conditions: Conditions { tags: &mut tags }};
+            assessor.assess();
+
+            // sammel referenznodes der ways und gib die koordinaten des ways an
+            let mut way_coords = Vec::new();
+            for node_id in way.refs() {
+                if let Some(&(lat, lon)) = node_coords.get(&node_id) {
+                    // bereite node-koordinaten in LineString-Format vor
+                    way_coords.push(coord! { x: lon, y: lat });
+                } else {
+                    // fallback
+                    println!("ref node_id {} not found for way_id {}", node_id, way.id());
+                }
+            }
+            // wandel die koordinaten in eine LineString-Repräsentation für csv-ausgabe aus
+            let way_coords_str = format!(
+                "LINESTRING ({})",
+                way_coords
+                    .iter()
+                    .map(|coord| format!("{} {}", coord.x, coord.y))
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+
+            // wandel die assesste infrastruktur in einen string um. falls None, dann leeres zeichen.
+            let infra = if let Some(infrastructure) = tags.get("bicycle_infrastructure") {
+                infrastructure.clone()
+            } else {
+                "".to_string()
+            };
+            // schreibe das ganze in eine csv.
+            wtr.write_record(&[way.id().to_string(), infra.to_string(), way_coords_str]);
+
+        }
+    })?;
+    // lösche den writer-zwischenspeicher
+    wtr.flush()?;
     Ok(())
 }
